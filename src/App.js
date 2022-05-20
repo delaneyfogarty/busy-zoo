a10847a14fc9a496909225f11217f5a2e56741bb
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-20 16:40:31 -0700

    got rid of unused imports

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Zoo from './Zoo';
 import AdminPage from './AdminPage';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
 
 export default function App() {
   return (
